44408a99d6af006cc37e94e45eda0dae63548518
Antonio <antoniojaa1802@gmail.com>
2023-09-01 17:12:49 -0300

    Add atividade

---
+++ aula05/src/aula/jdbc/Conexao.java
@@ -0,0 +1,30 @@
+package aula.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+	public static void main(String[] args){
+		 String driver = "com.mysql.cj.jdbc.Driver";
+		 String user = "root";
+		 String senha = "Isa29052003_Belly";
+		 String url = "jdbc:mysql://127.0.0.1:3306/aula05";
+
+		 try{
+			 Class.forName(driver);
+			 Connection con = null;
+
+			 con = (Connection) DriverManager.getConnection(url, user, senha);
+
+			 System.out.println("Conexão realizada com sucesso.");
+
+		 }
+		 catch (ClassNotFoundException ex){
+			 System.err.print(ex.getMessage());
+		 }
+		 catch (SQLException e){
+			 System.err.print(e.getMessage());
+		 }
+	 }
+}
---
+++ aula05/src/module-info.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+/**
+ * @author Isabelly Camilly
+ *
+ */
+module aula05 {
+	requires java.sql;
+	requires java.desktop;
+}
